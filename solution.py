ba2c1aa98f2a539786991eab857a61a94a1acc0f
vinyatu <107617394+vinyatu@users.noreply.github.com>
2023-04-04 12:51:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5991293770 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    sample_mean = np.mean(x)
+    sample_var = np.var(x)
+    a = np.log(sample_mean/np.sqrt(sample_var/sample_mean**2 +1))
+    return a # Ваш ответ
