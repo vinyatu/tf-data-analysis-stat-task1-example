315fc89a3814817793deab751839ebd596f1a6cc
vinyatu <107617394+vinyatu@users.noreply.github.com>
2023-04-04 17:53:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,7 @@ import numpy as np
 chat_id = 5991293770 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    sample_mean = np.mean(x)
-    sample_var = np.var(x)
-    a = np.log(sample_mean/np.sqrt(sample_var/sample_mean**2 +1))
-    return a # Ваш ответ
+    #sample_mean = np.mean(x)
+    #sample_var = np.var(x)
+    #a = np.log(sample_mean/np.sqrt(sample_var/sample_mean**2 +1))
+    return np.log(x-855).mean()
